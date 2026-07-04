b8a71b86ec070dfc66318c87c23ed2cbd450c0d8
0xRoger <roger1420h1@gmail.com>
2026-07-05 02:22:21 +0300

    s1
    Updated the JSON structure to remove duplicate server entries and ensure proper formatting.

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ $query = new SampQueryAPI('188.165.192.24', '9514');
 $aInformation = $query->getInfo(); 
 $aServerRules = $query->getRules(); 
 ?>
-{"version":1,"players_today": <?= $aInformation['players'] ?>,"1":{"serverid":1,"name":"HARD LIFE RP | 01 | TEXAS [TEST]","players":999,"max_players":1000,"bonus":0},"2":{"serverid":2,"name":"HOTLINE RP | TESgT","players":999,"max_players":1000,"bonus":0},"3":{"serverid":3,"name":"HOTLINE RP | TESgT","players":999,"max_players":1000,"bonus":0},"4":{"serverid":4,"name":"HOTLINE RP | TESgT","players":999,"max_players":1000,"bonus":0},"5":{"serverid":5,"name":"HOTLINE RP | TESgT","players":999,"max_players":1000,"bonus":0}}
+{"version":1,"players_today": <?= $aInformation['players'] ?>,"1":{"serverid":1,"name":"HARD LIFE RP | 01 | TEXAS [TEST]","players":999,"max_players":1000,"bonus":0}}
