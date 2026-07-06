b79a67c28ee4969bd3d07296038c7a7ae166a496
DevYlmazbytes <oopsinth3@gmail.com>
2026-07-07 00:33:47 +0300

    Update player count in server response

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ $query = new SampQueryAPI('188.165.192.24', '9514');
 $aInformation = $query->getInfo(); 
 $aServerRules = $query->getRules(); 
 ?>
-{"version":1,"players_today": <?= $aInformation['players'] ?>,"1":{"serverid":1,"name":APEX RP | 01 | DELTA ","players":999,"max_players":1000,"bonus":2}}
+{"version":1,"players_today": <?= $aInformation['players'] ?>,"1":{"serverid":1,"name":APEX RP | 01 | DELTA ","players":1,"max_players":1000,"bonus":2}}
